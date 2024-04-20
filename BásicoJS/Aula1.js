5f914e13c6580bb256eac2d7441eea79b028a7bf
Yago <162918182+YagoConstantino@users.noreply.github.com>
2024-04-20 18:48:29 -0300

    Aula1

---
+++ BásicoJS/Aula1.js
@@ -0,0 +1,8 @@
+console.log("ola 'mundo'"); // String
+console.log(1.5, 25, 6.5, 23); // int float
+nome = "Yago";
+console.log(
+  "Meu nome é ",
+  nome,
+  " e estou estudando JavaScript ás 18:35 de Sábado"
+);
