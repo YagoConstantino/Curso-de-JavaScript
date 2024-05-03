98c9faacd2b8c82fdc6b25c96f1b50e30c17db98
Yago <162918182+YagoConstantino@users.noreply.github.com>
2024-05-03 08:56:14 -0300

    Exercicio

---
+++ LogicaDeProg/AprendidoAteAqui.js
@@ -0,0 +1,7 @@
+const peso = document.querySelector("#peso");
+const altura = document.querySelector("#altura");
+
+const resultado = document.querySelector(".resultado");
+
+const imc = peso/(altura*altura);
+
