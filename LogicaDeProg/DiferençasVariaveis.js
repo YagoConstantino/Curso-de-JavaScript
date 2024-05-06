fb745ffeb040a34f99d073bb7a4d38cad2d21f16
Yago <162918182+YagoConstantino@users.noreply.github.com>
2024-05-06 10:45:25 -0300

    Diferenças entre Var/Let

--- LogicaDeProg/DiferençasVariaveis.js
+++ LogicaDeProg/DiferençasVariaveis.js
@@ -1,1 +1,27 @@
-//Difetenças entre Var e Let/Const//
+//Mais Difetenças entre Var e Let/Const//
+
+const verdade = true;
+
+let nome = 'yago';
+
+var nome2 = 'augusto';
+//Var só tem escopo de função
+
+var nome2 = 'Constantino' // Var pode ser redeclarado
+
+if (verdade)
+{
+    let nome = 'joao'; // Let tem escopo de bloco,logo pode haver outra variavel com o mesmo nome em outro bloco.{....bloco esta entre chaves}
+
+
+    console.log(nome,nome2);
+
+
+    if(verdade) // Bloco aninhado
+    {
+        let nome = 'Outro nome em outro bloco,todas são diferentes';
+
+        var nome2 = 'Ribeiro' // Var sendo redeclarada
+        console.log(nome,nome2)
+    }
+}    
