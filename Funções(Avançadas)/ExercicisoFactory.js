404d3cbf59a34036234d2668a8f319fbbb74081d
Yago <162918182+YagoConstantino@users.noreply.github.com>
2024-05-24 11:24:36 -0300

    Calculadora

--- Funções(Avançadas)/ExercicisoFactory.js
+++ Funções(Avançadas)/ExercicisoFactory.js
@@ -6,12 +6,60 @@ function criaCalculadora()
     return {
 
         display:document.querySelector(".display"),
+        btnClear:document.querySelector('.btn-clear'),
 
 
-
+        clearDisplay()
+        {
+            this.display.value="";
+        },
 
         inicia(){
             this.clickBotoes();
+            this.pressionaEnter();
+        },
+
+        pressionaEnter()
+        {
+            this.display.addEventListener('keyup', e =>{
+                if(e.keyCode===13)
+                    {
+                        this.equal();
+                    }
+                if(e.keyCode===46)
+                    {
+                        this.deleteOne();
+                    }
+            });
+        },
+
+        deleteOne()
+        {
+            this.display.value = this.display.value.slice(0,-1);
+        },
+
+        btnParaDisplay(valor)
+        {
+            this.display.value+=valor;
+        },
+
+        equal()
+        {
+            let conta = this.display.value;
+            try
+            {
+                conta = eval(conta);
+
+                if(!conta)
+                    {
+                        alert('Conta Invalida');
+                        return;
+                    }
+                this.display.value = String(conta);        
+            }catch(e){
+                alert("Conta Inválida");
+            }
+
         },
 
         clickBotoes()
@@ -22,13 +70,24 @@ function criaCalculadora()
                     {
                         this.btnParaDisplay(el.innerText)
                     }
+
+                if(el.classList.contains('btn-clear'))
+                    {
+                        this.clearDisplay();
+                    }
+                    
+                if(el.classList.contains('btn-del'))
+                    {
+                        this.deleteOne();
+                    }
+
+                if(el.classList.contains('btn-eq'))
+                    {
+                        this.equal();
+                    }    
+
             }.bind(this));
         },
-
-        btnParaDisplay(valor)
-        {
-            this.display.value+=valor;
-        }
     };
 }
 
