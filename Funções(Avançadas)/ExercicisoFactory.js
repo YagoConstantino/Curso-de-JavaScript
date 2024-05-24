e8be329541819bedb4b5afb56bad5d913103de66
Yago <162918182+YagoConstantino@users.noreply.github.com>
2024-05-24 11:02:15 -0300

    att

--- Funções(Avançadas)/ExercicisoFactory.js
+++ Funções(Avançadas)/ExercicisoFactory.js
@@ -1,1 +1,36 @@
 //Exercicio Calculadora
+
+function criaCalculadora()
+{
+
+    return {
+
+        display:document.querySelector(".display"),
+
+
+
+
+        inicia(){
+            this.clickBotoes();
+        },
+
+        clickBotoes()
+        {
+            document.addEventListener('click',function(e){
+                const el = e.target;
+                if(el.classList.contains('btn-num'))
+                    {
+                        this.btnParaDisplay(el.innerText)
+                    }
+            }.bind(this));
+        },
+
+        btnParaDisplay(valor)
+        {
+            this.display.value+=valor;
+        }
+    };
+}
+
+const calculadora = criaCalculadora();
+calculadora.inicia();
